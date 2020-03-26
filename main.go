aea7e722a6a3ab5b55a8600fb7c3d0d16eb5ba81
Mikhail Petrov <azalio@azalio.net>
2020-03-26 23:58:07 +0300

    add icq support

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 )
 
 var token = flag.String("token", "", "your token")
+var icqtoken = flag.String("icqtoken", "", "your icq token")
+var icqto string
+// var icqto = flag.String(&icqto, "icqto", "", "icq id")
+
+func init(){
+	flag.StringVar(&icqto, "icqto", "", "icq id")
+}
 
 // var isSandbox = flag.Bool("is_sandbox", true, "is sandbox env")
 
 func main() {
 	flag.Parse()
+	authICQ(*icqtoken)
 	rest()
 	// Chase()
 }
--- notify.go
+++ notify.go
@@ -1,3 +1,21 @@
 package main
 
-// https://www.tradingview.com/symbols/YNDX/
+import (
+	"github.com/mail-ru-im/bot-golang"
+	"log"
+)
+
+var bot *botgolang.Bot
+
+func authICQ(BOT_TOKEN string) {
+	var err error
+    bot, err = botgolang.NewBot(BOT_TOKEN)
+    if err != nil {
+        log.Println("wrong token")
+    }
+}
+
+func sendMessageICQ(text string, to string) {
+    message := bot.NewTextMessage(to, text)
+    message.Send()
+}
